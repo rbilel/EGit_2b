2d07cc6f26d57dfdd48e550805560d442486cc40
Mohamed Salah El Béji <mohamed.salah.elbeji@gmail.com>
2012-10-30 23:25:31 +0100

    --1--

--- Vente_aux_enchéres/src/tn/com/isamm/developpement/VenteAuxEncheres/dao/MembreDao.java
+++ Vente_aux_enchéres/src/tn/com/isamm/developpement/VenteAuxEncheres/dao/MembreDao.java
@@ -4,8 +4,7 @@ import tn.com.isamm.developpement.VenteAuxEncheres.model.Membre;
 
 public interface MembreDao {
 	
-	//Meeeeeeeeeeeeee
-	//aaaaaaaaaaaaaaaaaaaa
+
 	public void ajouterMembre(Membre membre);
 	public void modifierMembre (Membre membre);
 	

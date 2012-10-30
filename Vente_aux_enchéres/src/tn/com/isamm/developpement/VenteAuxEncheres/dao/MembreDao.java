30489d2c5cd31b8bcb6e1ab676a4e9ce3ae25fee
Mohamed Salah El Béji <mohamed.salah.elbeji@gmail.com>
2012-10-30 22:43:57 +0100

    first commit

---
+++ Vente_aux_enchéres/src/tn/com/isamm/developpement/VenteAuxEncheres/dao/MembreDao.java
@@ -0,0 +1,10 @@
+package tn.com.isamm.developpement.VenteAuxEncheres.dao;
+
+import tn.com.isamm.developpement.VenteAuxEncheres.model.Membre;
+
+public interface MembreDao {
+	
+	public void ajouterMembre(Membre membre);
+	public void modifierMembre (Membre membre);
+	
+}
---
+++ Vente_aux_enchéres/src/tn/com/isamm/developpement/VenteAuxEncheres/model/Objet.java
@@ -0,0 +1,136 @@
+package tn.com.isamm.developpement.VenteAuxEncheres.model;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * Entity implementation class for Entity: Objet
+ * 
+ */
+@Entity
+@Table(name = "Objet")
+public class Objet implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idObjet;
+	private long idVendeur;
+	private long idCat;
+	private String description;
+	private String prixDep;
+	private String prixAchatImmediat;
+	private long idAcheteur;
+	private String montantEnchereActuel;
+	@Temporal(TemporalType.DATE)
+	private Date dateDeb;
+	@Temporal(TemporalType.DATE)
+	private Date dateFin;
+	@OneToOne(cascade = CascadeType.MERGE)
+	private Membre membre;
+	private static final long serialVersionUID = 1L;
+
+	public Objet() {
+		super();
+	}
+
+	public long getIdObjet() {
+		return this.idObjet;
+	}
+
+	public void setIdObjet(long idObjet) {
+		this.idObjet = idObjet;
+	}
+
+	public long getIdVendeur() {
+		return this.idVendeur;
+	}
+
+	public void setIdVendeur(long idVendeur) {
+		this.idVendeur = idVendeur;
+	}
+
+	public long getIdCat() {
+		return this.idCat;
+	}
+
+	public void setIdCat(long idCat) {
+		this.idCat = idCat;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPrixDep() {
+		return this.prixDep;
+	}
+
+	public void setPrixDep(String prixDep) {
+		this.prixDep = prixDep;
+	}
+
+	public String getPrixAchatImmediat() {
+		return this.prixAchatImmediat;
+	}
+
+	public void setPrixAchatImmediat(String prixAchatImmediat) {
+		this.prixAchatImmediat = prixAchatImmediat;
+	}
+
+	public long getIdAcheteur() {
+		return this.idAcheteur;
+	}
+
+	public void setIdAcheteur(long idAcheteur) {
+		this.idAcheteur = idAcheteur;
+	}
+
+	public String getMontantEnchereActuel() {
+		return this.montantEnchereActuel;
+	}
+
+	public void setMontantEnchereActuel(String montantEnchereActuel) {
+		this.montantEnchereActuel = montantEnchereActuel;
+	}
+
+	public Membre getMembre() {
+		return membre;
+	}
+
+	public void setMembre(Membre membre) {
+		this.membre = membre;
+	}
+
+	public Date getDateDeb() {
+		return dateDeb;
+	}
+
+	public void setDateDeb(Date dateDeb) {
+		this.dateDeb = dateDeb;
+	}
+
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+	
+
+}

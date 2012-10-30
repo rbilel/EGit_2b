22800645564d9ec701b18edfdbcd90431a3df8db
rbilel <rbilel@live.fr>
2012-10-30 23:13:49 +0100

    aaaaaaaaaa

--- Vente_aux_enchéres/src/tn/com/isamm/developpement/VenteAuxEncheres/dao/MembreDao.java
+++ Vente_aux_enchéres/src/tn/com/isamm/developpement/VenteAuxEncheres/dao/MembreDao.java
@@ -4,6 +4,7 @@ import tn.com.isamm.developpement.VenteAuxEncheres.model.Membre;
 
 public interface MembreDao {
 	
+	//Meeeeeeeeeeeeee
 	public void ajouterMembre(Membre membre);
 	public void modifierMembre (Membre membre);
 	
